198df4d8792f2f29c73cf693f7e7a4c23b18f4c0
Natalia <nataliapanchenko@ukr.net>
2026-01-26 11:27:11 +0100

    Display Theme Colors, issue #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
+import { initialColors } from "./lib/colors";
 
 function App() {
   return (
     <>
       <h1>Theme Creator</h1>
-      // display colors here
+      {initialColors.map((color) => (
+        <Color
+          key={color.id}
+          color={color.hex}
+          role={color.role}
+          contrast={color.contrastText}
+        />
+      ))}
     </>
   );
 }
--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,5 +1,14 @@
 import "./Color.css";
 
-export default function Color({ color }) {
-  return null;
+export default function Color({ color, role, contrast }) {
+  return (
+    <div
+      className="color-card"
+      style={{ backgroundColor: color, color: contrast }}
+    >
+      <h2 className="color-card-headline">{color}</h2>
+      <p className="color-card-role">{role}</p>
+      <p>contrast: {contrast}</p>
+    </div>
+  );
 }
