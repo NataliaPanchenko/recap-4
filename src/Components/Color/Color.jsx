04a9eee2b0d10a03ad111731a7ecd514177d8325
Natalia <nataliapanchenko@ukr.net>
2026-01-27 14:18:47 +0100

    edit a color from the theme, issue #4

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,8 +1,23 @@
+import ColorForm from "../ColorForm/ColorForm";
 import "./Color.css";
 import { useState } from "react";
 
-export default function Color({ id, color, role, contrast, onDelete }) {
-  const [showConfirm, setShowConfirm] = useState(false);
+export default function Color({
+  id,
+  color,
+  role,
+  contrast,
+  onDelete,
+  onEditColor: onUpdateColor,
+}) {
+  const [showDelete, setShowDelete] = useState(false);
+  const [showEdit, setShowEdit] = useState(false);
+
+  function handleUpdateSubmit(updatedColor) {
+    onUpdateColor({ ...updatedColor, id });
+    setShowEdit(false);
+  }
+
   return (
     <div
       className="color-card"
@@ -11,14 +26,29 @@ export default function Color({ id, color, role, contrast, onDelete }) {
       <h2 className="color-card-headline">{color}</h2>
       <p className="color-card-role">{role}</p>
       <p>contrast: {contrast}</p>
-      {!showConfirm && (
-        <button onClick={() => setShowConfirm(true)}>Delete</button>
+      {!showDelete && (
+        <button onClick={() => setShowDelete(true)}>DELETE</button>
       )}
-      {showConfirm && (
+      {!showEdit && <button onClick={() => setShowEdit(true)}>EDIT</button>}
+      {showDelete && (
         <>
           <p className="color-card-highlight">Really delete?</p>
-          <button onClick={() => setShowConfirm(false)}>Cancel</button>
+          <button onClick={() => setShowDelete(false)}>Cancel</button>
           <button onClick={() => onDelete(id)}>Delete</button>
+        </>
+      )}
+      {showEdit && (
+        <>
+          <ColorForm
+            initialData={{
+              role: role,
+              hex: color,
+              contrastText: contrast,
+            }}
+            buttonText="UPDATE COLOR"
+            onSubmit={handleUpdateSubmit}
+          />
+          <button onClick={() => setShowEdit(false)}>CANCEL</button>
         </>
       )}
     </div>
